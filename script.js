027a13c774470472b2ea9cfbbe7fa394cf6a9438
MaddyKm <134648462+MaddyKm@users.noreply.github.com>
2023-08-26 12:10:09 -0400

    adds dom elements and starter code for HTML

---
+++ script.js
@@ -0,0 +1,25 @@
+//DEPENDENCIES~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//timer counts down from pressing start
+var timer = document.querySelector("#time");
+//question changes each time
+var questions = document.querySelector("#questions");
+//answers change each time
+var answers = document.querySelector("#answers");
+//pop up that says right or wrong and remaind for only 2 seconds
+//start quiz button
+var startquiz = document.querySelector("#startquiz");
+
+//DATA~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//highscores tracked
+//time (score) tracked
+//FUNCTIONS~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+//USER INTERACTIONS~~~~~~~~~~~~~~~~~~~~~~~~~~
+//start game button
+//click on the answer
+//switch to next question
+//display if last question was right
+//repeat until the last question
+//enter initials for high scores
+
+//INITIALIZATIONS~~~~~~~~~~~~~~~~~~~~~~~~~~~~
